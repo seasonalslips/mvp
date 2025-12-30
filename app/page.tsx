f2d5b8566419e36cfb10bc27a81a4c39bb403583
seasonalslips <hello@seasonalslips.com>
2025-12-30 11:04:13 -0600

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 24, fontFamily: "system-ui" }}>
-      <h1>SeasonalSlips (Next.js)</h1>
-      <p>Directory pages live under /marinas/...</p>
-    </main>
-  );
-}
