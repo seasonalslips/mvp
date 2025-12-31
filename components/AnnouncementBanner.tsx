6d8da1bc4c43764d8f63e5768921180a45ab0d74
seasonalslips <hello@seasonalslips.com>
2025-12-30 22:19:59 -0600

    Create AnnouncementBanner.tsx

---
+++ components/AnnouncementBanner.tsx
@@ -0,0 +1,7 @@
+export default function AnnouncementBanner() {
+  return (
+    <section style={{ padding: 12, background: "#f5f5f5" }}>
+      <p>Announcement banner placeholder</p>
+    </section>
+  );
+}
