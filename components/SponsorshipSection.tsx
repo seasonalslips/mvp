ed087f67b23bf6e58a37e4665c8b9a7409ceac93
seasonalslips <hello@seasonalslips.com>
2025-12-30 22:19:40 -0600

    Create SponsorshipSection.tsx

---
+++ components/SponsorshipSection.tsx
@@ -0,0 +1,8 @@
+export default function SponsorshipSection() {
+  return (
+    <section style={{ padding: 24 }}>
+      <h2>Sponsorships</h2>
+      <p>Sponsorship section placeholder</p>
+    </section>
+  );
+}
