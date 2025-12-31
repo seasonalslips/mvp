3d234c7c33270a5d57a6e1a9ffc64826741f7040
seasonalslips <hello@seasonalslips.com>
2025-12-30 22:16:35 -0600

    Update BlogPreview.tsx

--- components/BlogPreview.tsx
+++ components/BlogPreview.tsx
@@ -1,1 +1,9 @@
+export default function BlogPreview() {
+  return (
+    <section style={{ padding: 24 }}>
+      <h2>Blog Preview</h2>
+      <p>Blog preview placeholder</p>
+    </section>
+  );
+}
 
